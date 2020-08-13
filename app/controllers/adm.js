85dace785721dbe47577d14fdca54781c81d0dae
lucaskrispim <lucaskrispim@hotmail.com>
2020-08-13 11:11:25 -0300

    autenticaçao de admin

--- app/controllers/adm.js
+++ app/controllers/adm.js
@@ -1,15 +1,24 @@
 class Adm {
     static login(req, res) {
-        res.render('loginadm');
+        res.render('loginadm',{ validacao: []});
     }
 
     static autenticar(app, req, res) {
 
-        let connection = app.config.dbConnection(); // conexão com banco de dados
-        let eventosModel = new app.app.models.EventosDAO(connection); // instanciando a classe com métodos referentes ao banco de dados
-        eventosModel.getEventos((error, result) => { // Aqui usamos o método da classe para trazer todos os registros no banco de dados
-            res.render('crudadm', { eventos: result });
-        });
+        if (req.body.usuario == 'admin' && req.body.senha == 'admin') {
+            req.session.autorizado = true;
+            req.session.usuario = 'admin';
+            req.session.senha = 'admin';
+            let connection = app.config.dbConnection(); // conexão com banco de dados
+            let eventosModel = new app.app.models.EventosDAO(connection); // instanciando a classe com métodos referentes ao banco de dados
+            eventosModel.getEventos((error, result) => { // Aqui usamos o método da classe para trazer todos os registros no banco de dados
+                res.render('crudadm', { eventos: result });
+            });
+        }else {
+            console.log(req.body)
+            res.render('loginadm',{ validacao: [{ msg: 'Este nome de usuário não está cadastrado no banco de dados!' }]});   
+        }
+
     }
 }
 
--- app/routes/home.js
+++ app/routes/home.js
@@ -2,11 +2,6 @@ module.exports = (app) =>{ // rota da inserção externa de dados na aplicação
     app.get('/',(req,res)=>{
       app.app.controllers.home.index(app,req,res);
     });
-
-    app.post('/autenticar',(req,res)=>{
-      app.app.controllers.home.autenticar(app,req,res);
-    });
-
 };
 
 
